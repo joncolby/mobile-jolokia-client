b7d8e19f55423b102de70280a3b3e297bd138202
Jonathan Colby <jcolby@team.mobile.de>
2013-04-24 13:54:21 +0200

    debian package files

---
+++ package/mobile-jolokia-client-1.0/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(mobile-jolokia-client):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="mobile-jolokia-client" command="/usr/bin/mobile-jolokia-client"
---
+++ package/mobile-jolokia-client-1.0/debian/mobile-jolokia-client.default.ex
@@ -0,0 +1,10 @@
+# Defaults for mobile-jolokia-client initscript
+# sourced by /etc/init.d/mobile-jolokia-client
+# installed at /etc/default/mobile-jolokia-client by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
