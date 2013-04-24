e95e2d5881337a142bc09516ddcf6848b4a6bd7c
Jonathan Colby <jcolby@ebay.de>
2013-04-24 16:21:49 +0200

    enahnce search client

--- src/main/groovy/de/mobile/siteops/jolokia/JolokiaSearchClient.groovy
+++ src/main/groovy/de/mobile/siteops/jolokia/JolokiaSearchClient.groovy
@@ -13,7 +13,7 @@ cli.with {
     h(longOpt: 'host', required: true, args: 1, 'remote host')
     p(longOpt: 'port', required: true, args: 1, 'remote port')
     b(longOpt: 'bean', required: true, args: 1, 'jmx bean name')
-    //a(longOpt: 'attribute', required: false, args: Option.UNLIMITED_VALUES, type: String, valueSeparator: ',', 'seperate multiple attributes with comma.')
+    a(longOpt: 'attribute', required: false, args: Option.UNLIMITED_VALUES, type: String, valueSeparator: ',', 'seperate multiple attributes with comma.')
 }
 
 def opt = cli.parse(args)
@@ -29,6 +29,15 @@ if (opt.'?')
     def host = opt.h
     def port = opt.p
     def bean = opt.b
+    def attributes = opt.as
+    def attribute = opt.a
+
+    def attrListString
+
+    if (attributes)
+        attrListString = attributes.join(',')
+    else
+        attrListString = ""
 
     try {
 
@@ -45,12 +54,21 @@ if (opt.'?')
     //J4pReadResponse response = client.execute(request);
      J4pSearchResponse response = client.execute(request);
 
+
+
+
             response.objectNames.each { ObjectName on ->
-                       J4pReadRequest req = new J4pReadRequest(on,"")
+                       J4pReadRequest req = new J4pReadRequest(on,"${attrListString}")
                        Map requestParameters = [:]
                         requestParameters[J4pQueryParameter.IGNORE_ERRORS] = "true"
                        J4pReadResponse res = client.execute(req,requestParameters)
-                       res.getValue().each { k,v ->  println k + "=" + v}
+
+                       def responseValue =   res.getValue()
+
+                        if (responseValue instanceof Map)
+                                responseValue.each { k,v ->  println k + "=" + v}
+                        else
+                                println attribute + "=" + responseValue
             }
 
         /*
